ba556266af6aec90817f4f4686d1c37a9d8db27a
azemcov <111581518+azemcov@users.noreply.github.com>
2022-10-05 21:47:17 +0500

    Add files via upload

---
+++ code.js
@@ -0,0 +1,12 @@
+var x = document.getElementById('x')
+var y = document.getElementById('y')
+
+function rx()
+{
+document.getElementById('box').style.transform = 'rotateX('+x.value+'deg) rotateY('+y.value+'deg)';
+}
+
+function ry()
+{
+document.getElementById('box').style.transform = 'rotateX('+x.value+'deg) rotateY('+y.value+'deg)';
+}
